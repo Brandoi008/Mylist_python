35fbe0a38ffedf0fb26818081326443c4891ea59
Brandoi008 <makutobrian1@gmail.com>
2024-03-03 15:08:07 -0400

    Second commit

--- my_list.py
+++ my_list.py
@@ -1,23 +1,17 @@
-# 1. Create an empty list
+#Empty list
 my_list = []
- 
-# 2. Append elements 
+  
 my_list.append(10)
 my_list.append(20)
 my_list.append(30) 
 my_list.append(40)
 
-# 3. Insert 15 at index 1
 my_list.insert(1, 15)  
 
-# 4. Extend with another list
 my_list.extend([50, 60, 70])  
 
-# 5. Remove last element
 my_list.pop(-1)  
 
-# 6. Sort list  
 my_list.sort()
 
-# 7. Print index of 30
 print(my_list.index(30))
